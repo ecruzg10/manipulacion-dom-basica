fcf6701e4436b2b6dfbd565dca927966f344fbdb
ecruzg10 <ecruzg10@gmail.com>
2023-03-16 11:44:58 -0600

    Eventos desde Jscript con addeventlistener

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const h1 =document.querySelector('h1');
 const input1 = document.querySelector ('#calculo1');
 const input2 = document.querySelector ('#calculo2');
-const btn = document.querySelector('#btncalcular');
+const btn = document.querySelector('#btnCalcular');
 const pResult = document.querySelector('#result');
+const form = document.querySelector('#form')
+
+
+ btn.addEventListener('click',btnOnClick);
+
 
 function btnOnClick()
 {
     const sumaInputs = parseInt(input1.value)+ parseInt(input2.value);
-    pResult.innerText= sumaInputs;
+     pResult.innerText= sumaInputs;
 }
